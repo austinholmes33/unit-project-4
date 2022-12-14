3534f4286e4b0d711fc987ffb5073b53f9714462
Austin Holmes <austin.holmes4@gmail.com>
2022-12-14 12:33:59 -0700

    part 4 done

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField, BooleanField
 from wtforms.validators import DataRequired, Length
 
 
@@ -7,3 +7,12 @@ class TeamForm(FlaskForm):
     team_name = StringField("team name", validators=[DataRequired(), Length(min=4, max=255)])
     submit = SubmitField("submit")
 
+class ProjectForm(FlaskForm):
+    project_name = StringField("project name", validators=[DataRequired(), Length(min=4, max=255)])
+    description = StringField("description")
+    completed = BooleanField("completed?")
+    team_selection = SelectField("team")
+    submit = SubmitField("submit")
+
+    def update_teams(self, teams):
+        self.team_selection.choices = [ (team.id, team.team_name) for team in teams]
--- server.py
+++ server.py
@@ -1,13 +1,36 @@
-from flask import Flask, render_template
-from forms import TeamForm
+from flask import Flask, render_template, url_for, redirect
+from forms import TeamForm, ProjectForm
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     team_form = TeamForm()
+    project_form = ProjectForm()
+    project_form.update_teams(User.query.get(user_id).teams)
+    return render_template("home.html", team_form = team_form, project_form=project_form)
+
+@app.route("/add-team", methods=["POST"])
+def add_team():
+    team_form = TeamForm()
+
+    if team_form.validate_on_submit():
+        print(team_form.team_name.data)
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("home"))
+
+@app.route("/add-project", methods=["POST"])
+def add_project():
+    project_form = ProjectForm()
+    project_form.update_teams(User.query.get(user_id).teams)
     
-    return render_template("home.html")
+    if project_form.validate_on_submit():
+        print(project_form.project_name.data)
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("home"))
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
